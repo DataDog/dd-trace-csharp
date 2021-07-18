2f094c5c955ba800e84676295a8043714d06a5e2
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2021-07-18 10:07:04 +0200

    Make getPathName inline to avoid multiple redefinitions error

--- src/Datadog.Trace.ClrProfiler.Native/logger_impl.h
+++ src/Datadog.Trace.ClrProfiler.Native/logger_impl.h
@@ -64,7 +64,7 @@ private:
 
 #ifndef _WIN32
 // for linux and osx we need a function to get the path from a filepath
-std::string getPathName(const std::string& s)
+inline std::string getPathName(const std::string& s)
 {
     char sep = '/';
     size_t i = s.rfind(sep, s.length());
